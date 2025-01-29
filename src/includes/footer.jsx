5bed29cf131573eeec64969beb8676b3ea6bf0a2
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2025-01-29 11:52:44 +0100

    go

--- src/includes/footer.jsx
+++ src/includes/footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
                                         {/* <!-- social --> */}
                                         <div className="footer-social">
                                             <a href="#"><i className="fab fa-twitter-square"></i></a>
-                                            <a href="https://bit.ly/sai4ull"><i className="fab fa-facebook-square"></i></a>
+                                            <a href="https://www.facebook.com/share/15eynNSxri/"><i className="fab fa-facebook-square"></i></a>
                                             <a href="#"><i className="fab fa-linkedin"></i></a>
                                             <a href="#"><i className="fab fa-pinterest-square"></i></a>
                                         </div>
