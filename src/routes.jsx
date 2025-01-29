f44ff92a3ac9cfdc9e7cdbdf00d0dc329469ea75
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2025-01-29 11:00:08 +0100

    go

---
+++ src/routes.jsx
@@ -0,0 +1,14 @@
+import Home from "./pages/home/home";
+
+const routes = [
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/home",
+        element: <Home />,
+    }
+];
+
+export default routes;
