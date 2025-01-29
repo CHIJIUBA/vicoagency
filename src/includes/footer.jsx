2e83b8533088f3a509fb36a696f64b07dd60e842
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2025-01-29 12:02:07 +0100

    go

--- src/includes/footer.jsx
+++ src/includes/footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
                                         <div className="footer-social">
                                             <a href="#"><i className="fab fa-twitter-square"></i></a>
                                             <a href="https://www.facebook.com/share/15eynNSxri/"><i className="fab fa-facebook-square"></i></a>
-                                            <a href="#"><i className="fab fa-linkedin"></i></a>
+                                            <a href="https://www.linkedin.com/posts/vico-digital-agency_digitalagency-businesspeople-creativemarketingsolution-activity-7289627798720106496-CRJ6?utm_source=share&utm_medium=member_android"><i className="fab fa-linkedin"></i></a>
                                             <a href="#"><i className="fab fa-pinterest-square"></i></a>
                                         </div>
                                     </div>
